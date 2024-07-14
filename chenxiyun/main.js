3d1400f99430256b1f4a121eb94677ed12dc5585
Jaeden Tai <taijaeden@gmail.com>
2024-07-14 20:52:10 +0800

    js web

---
+++ chenxiyun/main.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("nav").style.height = "500px";
+    document.getElementById("nav").style.visibility = "visible";
+}
+function closeNav() {
+    document.getElementById("nav").style.visibility = "hidden";
+    document.getElementById("nav").style.height = "0";
+}
